0c71bf9ac8f9a160dc619ca2cc9e6e6b8120d35f
Amira Walid <amirawaleed012@gmail.com>
2024-10-06 00:30:15 +0300

    Iterating through report objects

---
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -0,0 +1,7 @@
+export default function createIteratorObject(report) {
+  const allEmployees = [];
+  for (const employees of Object.values(report.allEmployees)) {
+    allEmployees.push(...employees);
+  }
+  return allEmployees;
+}
