15e14adce75ee5758b6aacc30e0af99e40e6795b
Amira Walid <amirawaleed012@gmail.com>
2024-10-21 18:01:04 +0300

    Combine

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,11 +1,12 @@
-/* eslint-disable no-param-reassign */
 function updateStudentGradeByCity(students, city, newGrades) {
   return students
     .filter((student) => student.location === city)
     .map((student) => {
-      const matchStudent = newGrades.filter((ele) => ele.studentId === student.id);
-      student.grade = matchStudent[0] ? matchStudent[0].grade : 'N/A';
-      return student;
+      const matchStudent = newGrades.find((grade) => grade.studentId === student.id);
+      return {
+        ...student,
+        grade: matchStudent ? matchStudent.grade : 'N/A',
+      };
     });
 }
 module.exports = updateStudentGradeByCity;
