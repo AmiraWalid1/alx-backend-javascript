8213d1c923ab2dd10a060611d028e1c4ff71f287
Amira Walid <amirawaleed012@gmail.com>
2024-12-20 00:27:37 +0200

    stdin

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,13 +1,10 @@
-// Display the welcome message
 process.stdout.write('Welcome to ALX, what is your name?\n');
 
-// Listen for user input
 process.stdin.on('data', (data) => {
   const name = data.toString().trim();
   console.log(`Your name is: ${name}`);
 });
 
-// Handle program closure
 process.stdin.on('end', () => {
   console.log('This important software is now closing');
 });
